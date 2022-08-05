4ae27ca750218beb82dbd4b351beee946347f056
Burak Diker <burak.diker@ug.bilkent.edu.tr>
2022-08-05 23:42:18 +0300

    added-sticky-nav

--- script.js
+++ script.js
@@ -27,3 +27,25 @@ allLinks.forEach(function (link) {
     }
   });
 });
+
+// sticky menu
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    // console.log(ent);
+    if(!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if(ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
